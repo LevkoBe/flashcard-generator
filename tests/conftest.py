d93da75086ee9538d18556dbe5d90c2e6e45bc50
LevkoBe <lbeniakh@kse.org.ua>
2025-10-12 23:34:32 +0300

    fix: git workflow env vars

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,10 +10,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TEST_DATABASE_URL = os.getenv(
-    "TEST_DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/test_flashcard_generator"
-)
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
 
 engine = create_engine(TEST_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
